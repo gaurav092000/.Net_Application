a5e2a3a563e7112e1dc337410fd9794c1a2567a5
gaurav2000 <gauravkawde@nimapinfotech.com>
2023-07-18 18:52:59 +0530

    18-07-23

---
+++ My_First_MvC_App/ViewData_Demo/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ViewData_Demo.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            ViewData["Message"] = "Message From the View Data!!!";
+            ViewData["Currunt Time :"] = DateTime.Now.ToLongDateString();
+
+            string[] fruit = { "Apple","Mango","Banana"};
+            ViewData["FruitArray"]= fruit;
+
+            ViewData["SportData"] = new List<string>()
+            {
+                "Cricket",
+                "Hokey",
+                "BasketBall"
+
+            };
+
+            return View();
+        }
+    }
+}
---
+++ My_First_MvC_App/WebApplication3/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication3.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public string show()
+        {
+            return "This My second index page ";
+        }
+        public ActionResult AboutUs()
+        {
+            return View();
+        }
+        public int StudentId(int id )
+        {
+            return id;
+        }
+
+    }
+}
